86e24f55a0a6dfffe665fc603ff8f7731d828475
Attila Fazekas <fazekas.attila33@gmail.com>
2023-07-02 22:05:40 +0200

    Add binary-compatibility-validator plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation("com.github.johnrengelman.shadow:com.github.johnrengelman.shadow.gradle.plugin:_")
     implementation("io.kotest:io.kotest.gradle.plugin:_")
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:_")
+    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:_")
 }
 
 kotlin {
