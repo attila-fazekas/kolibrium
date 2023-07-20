6366e8c4b16094a9e95b80977dc79208c4d5c144
Attila Fazekas <fazekas.attila33@gmail.com>
2023-07-20 22:04:48 +0200

    Add and configure Jreleaser. Fix typos (#5)

--- buildSrc/src/main/kotlin/kolibrium.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kolibrium.library-conventions.gradle.kts
@@ -1,12 +1,57 @@
+import org.jreleaser.model.Active.ALWAYS
+
 plugins {
     `java-library`
     `maven-publish`
     id("kolibrium.base-conventions")
     id("com.github.johnrengelman.shadow")
+    id("org.jreleaser")
 }
 
 kotlin {
     explicitApi()
+}
+
+jreleaser {
+    dryrun = true
+    project {
+        author("Attila Fazekas")
+        copyright = "Copyright © 2023 Attila Fazekas. All rights reserved."
+        description = "Kotlin library for Selenium tests"
+        gitRootSearch = true
+        inceptionYear = "2023"
+        license = "Apache-2.0"
+        links {
+            documentation = "https://github.com/attila-fazekas/kolibrium"
+            homepage = "https://github.com/attila-fazekas/kolibrium"
+            license = "http://www.apache.org/licenses/LICENSE-2.0"
+        }
+    }
+    release {
+        github {
+            changelog {
+                contributors {
+                    enabled = false
+                }
+                format = "{{commitShortHash}} {{commitTitle}}"
+                formatted = ALWAYS
+                skipMergeCommits = true
+            }
+            overwrite = true
+            releaseName = "Kolibrium {{projectVersionNumber}}"
+            repoOwner = "attila-fazekas"
+            tagName = "v{{projectVersionNumber}}"
+        }
+    }
+    distributions {
+        subprojects.forEach {
+            create(it.name) {
+                artifact {
+                    path.set(file("/build/libs/{{distributionName}}-{{projectVersion}}.jar"))
+                }
+            }
+        }
+    }
 }
 
 publishing {
--- core/src/main/kotlin/io/kolibrium/core/Error.kt
+++ core/src/main/kotlin/io/kolibrium/core/Error.kt
@@ -18,6 +18,6 @@ package io.kolibrium.core
 
 public sealed class Error(override val message: String) : Throwable() {
     public class ElementNotFound(locator: String) : Error("Element \"$locator\" could not be found.")
-    public object UnreachableBrowser : Error("Browser is unreachable.")
+    public object UnreachableBrowser : Error("Browser was unreachable.")
     public object NoSuchWindow : Error("Browser window/tab was not found.")
 }
