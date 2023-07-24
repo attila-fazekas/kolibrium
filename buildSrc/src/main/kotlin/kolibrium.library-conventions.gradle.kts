48537b2d36eb36cfe5df91c44069e734b52f4e53
Attila Fazekas <fazekas.attila33@gmail.com>
2023-07-24 17:57:18 +0200

    Configure Jreleaser for publishing to Maven Central

--- buildSrc/src/main/kotlin/kolibrium.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kolibrium.library-conventions.gradle.kts
@@ -12,6 +12,67 @@ kotlin {
     explicitApi()
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+val projectGitUrl = "https://github.com/attila-fazekas/kolibrium"
+val scm = "scm:git:$projectGitUrl.git"
+val stagingDir = layout.buildDirectory.dir("staging-deploy")
+
+val javadocJar by tasks.register<Jar>("dokkaHtmlJar") {
+    group = "documentation"
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+val sourcesJar by tasks.named("sourcesJar")
+
+publishing {
+    publications {
+        create<MavenPublication>("kolibrium") {
+            from(components["kotlin"])
+            artifact(javadocJar)
+            artifact(sourcesJar)
+            artifactId = "$name-$artifactId"
+            pom {
+                name = rootProject.name
+                description = "Kotlin library for Selenium tests"
+                inceptionYear = "2023"
+                url = projectGitUrl
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "attila-fazekas"
+                        name = "Attila Fazekas"
+                    }
+                }
+                issueManagement {
+                    system = "GitHub"
+                    url = "$projectGitUrl/issues"
+                }
+                scm {
+                    connection = scm
+                    developerConnection = scm
+                    url = projectGitUrl
+                }
+            }
+        }
+        repositories {
+            maven {
+                url = uri(stagingDir)
+            }
+        }
+    }
+}
+
 jreleaser {
     dryrun = true
     project {
@@ -22,8 +83,8 @@ jreleaser {
         inceptionYear = "2023"
         license = "Apache-2.0"
         links {
-            documentation = "https://github.com/attila-fazekas/kolibrium"
-            homepage = "https://github.com/attila-fazekas/kolibrium"
+            documentation = projectGitUrl
+            homepage = projectGitUrl
             license = "http://www.apache.org/licenses/LICENSE-2.0"
         }
     }
@@ -40,25 +101,25 @@ jreleaser {
             overwrite = true
             releaseName = "Kolibrium {{projectVersionNumber}}"
             repoOwner = "attila-fazekas"
-            tagName = "v{{projectVersionNumber}}"
         }
     }
-    distributions {
-        subprojects.forEach {
-            create(it.name) {
-                artifact {
-                    path.set(file("/build/libs/{{distributionName}}-{{projectVersion}}.jar"))
+    signing {
+        active = ALWAYS
+        armored = true
+    }
+    deploy {
+        maven {
+            nexus2 {
+                register("maven-central") {
+                    active = ALWAYS
+                    group = "io.github.attila-fazekas"
+                    closeRepository = true
+                    releaseRepository = true
+                    snapshotUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                    url = "https://s01.oss.sonatype.org/service/local"
+                    stagingRepository(stagingDir.get().toString())
                 }
             }
-        }
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("kolibrium") {
-            artifactId = "$name-$artifactId"
-            from(components["kotlin"])
         }
     }
 }
