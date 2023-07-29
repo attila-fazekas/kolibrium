612edc007e264cddb96b4f7b9166d40eab6ad9cd
Attila Fazekas <fazekas.attila33@gmail.com>
2023-07-29 12:59:11 +0200

    Configure refreshVersions (#7)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,14 @@
+import de.fayard.refreshVersions.core.StabilityLevel
+
 rootProject.name = "kolibrium"
 include("core")
 
 plugins {
     id("de.fayard.refreshVersions") version "0.51.0"
+}
+
+refreshVersions {
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
 }
