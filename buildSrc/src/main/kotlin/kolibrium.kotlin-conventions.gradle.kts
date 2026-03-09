0d429a97f178226968c7edefc21050d715f85085
Attila Fazekas <fazekas.attila33@gmail.com>
2026-03-09 22:36:02 +0100

    Update year in copyright header (#373)
    Signed-off-by: Attila Fazekas <fazekas.attila33@gmail.com>

--- buildSrc/src/main/kotlin/kolibrium.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kolibrium.kotlin-conventions.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /*
- * Copyright 2023-2025 Attila Fazekas & contributors
+ * Copyright 2023-2026 Attila Fazekas & contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- selenium/src/test/kotlin/dev/kolibrium/dsl/seleniumTest/saucedemo/SauceDemoTest.kt
+++ selenium/src/test/kotlin/dev/kolibrium/dsl/seleniumTest/saucedemo/SauceDemoTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 Attila Fazekas & contributors
+ * Copyright 2023-2026 Attila Fazekas & contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 @file:Suppress("ktlint")
 
 /*
- * Copyright 2023-2025 Attila Fazekas & contributors
+ * Copyright 2023-2026 Attila Fazekas & contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
